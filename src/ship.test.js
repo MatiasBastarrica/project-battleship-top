d4a0795d71d7927ea8d55211f079d245d3fc3005
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-03-17 11:59:13 -0300

    Test the "hit" function of the "Ship" class

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,2 +1,14 @@
 import { describe, expect, test } from "@jest/globals";
 import { Ship } from "./ship.js";
+
+test("Take hits", () => {
+  const hits = [1, 2, 3, 4];
+
+  hits.forEach((hit) => {
+    let ship = new Ship(3);
+    for (let i = 0; i < hit; i++) {
+      ship.hit();
+    }
+    expect(ship.hitCount).toBe(hit);
+  });
+});

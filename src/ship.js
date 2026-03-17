4f08c2b4d31a30da656123025a02b6032a4fc19e
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-03-17 11:30:19 -0300

    Add a hit function the the Ship class

--- src/ship.js
+++ src/ship.js
@@ -1,7 +1,11 @@
 class Ship {
   constructor(length) {
     this.length = length;
-    this.hitCout = 0;
+    this.hitCount = 0;
     this.sunk = false;
+  }
+
+  hit() {
+    this.hitCount++;
   }
 }

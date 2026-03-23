f730f1ab4c7247e7900233f0f16ff6e2926c3dcd
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-03-23 20:08:15 -0300

    Write a function for the computer to play a turn

--- src/game.js
+++ src/game.js
@@ -8,6 +8,7 @@ export class Game {
   }
 
   over = false;
+  #computerAttacks = [];
 
   playTurn(coordinate) {
     let opponent = this.turn === this.player1 ? this.computer : this.player1;
@@ -23,5 +24,19 @@ export class Game {
   gameOver(winner) {
     this.over = true;
     console.log(`${winner.name} is the winner!!!`);
+  }
+
+  playComputerTurn() {
+    let row = getRandomNumInRange(0, 9);
+    let col = getRandomNumInRange(0, 9);
+
+    while (computerAttacks.includes(String([row, col]))) {
+      row = getRandomNumInRange(0, 9);
+      col = getRandomNumInRange(0, 9);
+    }
+
+    this.#computerAttacks.push(String([row, col]));
+
+    this.playTurn([row, col]);
   }
 }

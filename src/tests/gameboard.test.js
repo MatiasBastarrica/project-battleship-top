9c3453ab31a57d217f773a52e57142bb730a6a4c
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-03-29 11:44:28 -0300

    Refactor the tests after the changes on the "gameboard.js" module

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,45 +1,51 @@
 import { describe, expect, test } from "@jest/globals";
 import { Gameboard } from "../gameboard.js";
 
-test.skip("Receive one attack", () => {
+test("Receive one attack", () => {
   let gameboard1 = new Gameboard();
-  gameboard1.placeShip([0, 1], gameboard1.ships.carrier, "horizontal");
-  gameboard1.receiveAttack([0, 1]);
+  gameboard1.placeShip([0, 1], gameboard1.ships.carrier, "x");
+  gameboard1.receiveAttack([0, 1], "Leon S Kennedy");
   expect(gameboard1.board[0][1].ship.hitCount).toBe(1);
 });
 
-test.skip("Check if there are shinks remaining", () => {
+test("Check if there are shinks remaining", () => {
   let gameboard1 = new Gameboard();
-  gameboard1.placeShip([0, 0], gameboard1.ships.carrier, "horizontal");
-  gameboard1.placeShip([1, 0], gameboard1.ships.patrolBoat, "horizontal");
-  gameboard1.receiveAttack([0, 0]);
-  gameboard1.receiveAttack([0, 1]);
-  gameboard1.receiveAttack([1, 0]);
-  gameboard1.receiveAttack([1, 1]);
+  gameboard1.placeShip([0, 0], gameboard1.ships.carrier, "x");
+  gameboard1.placeShip([1, 0], gameboard1.ships.patrolBoat, "x");
+  gameboard1.receiveAttack([0, 0], "Leon S Kennedy");
+  gameboard1.receiveAttack([0, 1], "Leon S Kennedy");
+  gameboard1.receiveAttack([1, 0], "Leon S Kennedy");
+  gameboard1.receiveAttack([1, 1], "Leon S Kennedy");
   expect(gameboard1.getAllSunkReport()).toBe(false);
 });
 
-test.skip("Check if all the ships have been sunk", () => {
+test("Check if all the ships have been sunk", () => {
   let gameboard1 = new Gameboard();
-  function sink(ship, board, coordinate) {
+  function sink(ship, board, coordinate, axis) {
+    let row = coordinate[0];
+    let col = coordinate[1];
     for (let i = 0; i < ship.length; i++) {
-      board.receiveAttack(coordinate);
+      if (axis === "x") {
+        board.receiveAttack([row, col + i], "Leon S Kennedy");
+      } else {
+        board.receiveAttack([row + i, col], "Leon S Kennedy");
+      }
     }
   }
-  gameboard1.placeShip([0, 0], gameboard1.ships.carrier, "horizontal");
-  sink(gameboard1.ships.carrier, gameboard1, [0, 0]);
-  gameboard1.placeShip([1, 0], gameboard1.ships.battleship, "horizontal");
-  sink(gameboard1.ships.battleship, gameboard1, [1, 0]);
-  gameboard1.placeShip([2, 0], gameboard1.ships.destroyer, "horizontal");
-  sink(gameboard1.ships.destroyer, gameboard1, [2, 0]);
-  gameboard1.placeShip([3, 0], gameboard1.ships.submarine, "horizontal");
-  sink(gameboard1.ships.submarine, gameboard1, [3, 0]);
-  gameboard1.placeShip([4, 0], gameboard1.ships.patrolBoat, "horizontal");
-  sink(gameboard1.ships.patrolBoat, gameboard1, [4, 0]);
+  gameboard1.placeShip([0, 0], gameboard1.ships.carrier, "x");
+  sink(gameboard1.ships.carrier, gameboard1, [0, 0], "x");
+  gameboard1.placeShip([1, 0], gameboard1.ships.battleship, "x");
+  sink(gameboard1.ships.battleship, gameboard1, [1, 0], "x");
+  gameboard1.placeShip([2, 0], gameboard1.ships.destroyer, "x");
+  sink(gameboard1.ships.destroyer, gameboard1, [2, 0], "x");
+  gameboard1.placeShip([3, 0], gameboard1.ships.submarine, "x");
+  sink(gameboard1.ships.submarine, gameboard1, [3, 0], "x");
+  gameboard1.placeShip([4, 0], gameboard1.ships.patrolBoat, "x");
+  sink(gameboard1.ships.patrolBoat, gameboard1, [4, 0], "x");
   expect(gameboard1.getAllSunkReport()).toBe(true);
 });
 
-test.skip("Attack the same coordinate", () => {
+test("Attack the same coordinate", () => {
   const gameboard1 = new Gameboard();
   gameboard1.placeShip([0, 0], gameboard1.ships.carrier, "x");
   gameboard1.receiveAttack([0, 0]);

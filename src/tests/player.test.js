291548b16c243fd97fa539c329e720ad60ccad52
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-03-30 12:37:45 -0300

    Test wether both players hit water

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, test } from "@jest/globals";
 import { RealPlayer, Computer } from "../player.js";
 
-test("Real player plays one turn", () => {
+test("Real player plays one turn hitting a ship", () => {
   let real = new RealPlayer("Leon S Kennedy");
   let computer = new Computer();
   computer.gameboard.placeShip([0, 0], computer.gameboard.ships.carrier, "x");
@@ -9,10 +9,32 @@ test("Real player plays one turn", () => {
   expect(computer.gameboard.board[0][0].ship.hitCount).toBe(1);
 });
 
-test("Computer plays one turn", () => {
+test("Real player plays one turn hitting  water", () => {
+  let real = new RealPlayer("Leon S Kennedy");
+  let computer = new Computer();
+  computer.gameboard.placeShip([0, 0], computer.gameboard.ships.carrier, "x");
+  real.playTurn(computer, [1, 0]);
+  expect(
+    !computer.gameboard.board[1][0].ship &&
+      !computer.gameboard.board[1][0].alreadyAttacked,
+  ).toBe(true);
+});
+
+test("Computer plays one turn hitting a ship", () => {
   let real = new RealPlayer("Leon S Kennedy");
   let computer = new Computer();
   real.gameboard.placeShip([0, 0], computer.gameboard.ships.carrier, "x");
   computer.playTurn(real, [0, 0]);
   expect(real.gameboard.board[0][0].ship.hitCount).toBe(1);
+});
+
+test("Computer plays one turn hitting water", () => {
+  let real = new RealPlayer("Leon S Kennedy");
+  let computer = new Computer();
+  real.gameboard.placeShip([0, 0], computer.gameboard.ships.carrier, "x");
+  computer.playTurn(real, [1, 0]);
+  expect(
+    !real.gameboard.board[1][0].ship &&
+      !real.gameboard.board[1][0].alreadyAttacked,
+  ).toBe(true);
 });

5dcbc94078716a89bacb6901c1ae9b2c3f716621
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-03-30 19:56:24 -0300

    Test wether the winner is the computer

--- src/tests/game.test.js
+++ src/tests/game.test.js
@@ -2,7 +2,7 @@ import { describe, expect, test } from "@jest/globals";
 import { Game } from "../game.js";
 import { getRandomNumInRange } from "../utils.js";
 
-test("Play a game", () => {
+test.skip("Play a game", () => {
   const game1 = new Game("Leon S Kennedy");
   game1.player1.gameboard.placeAllRandom();
   game1.computer.gameboard.placeAllRandom();
@@ -22,4 +22,26 @@ test("Play a game", () => {
   }
 
   expect(game1.over).toBe(true);
+});
+
+test("Play a game in which computer wins", () => {
+  const game1 = new Game("Leon S Kennedy");
+  game1.player1.gameboard.placeAllRandom();
+  game1.computer.gameboard.placeAllRandom();
+
+  let player1Attacks = [];
+  let computerAttacks = [];
+
+  while (!game1.over) {
+    let coor = [getRandomNumInRange(0, 9), getRandomNumInRange(0, 9)];
+    if (game1.turn === game1.player1) {
+      while (player1Attacks.includes(String(coor))) {
+        coor = [getRandomNumInRange(0, 9), getRandomNumInRange(0, 9)];
+      }
+      player1Attacks.push(String(coor));
+    }
+    game1.play(coor);
+  }
+
+  expect(game1.winner).toBe("Computer");
 });

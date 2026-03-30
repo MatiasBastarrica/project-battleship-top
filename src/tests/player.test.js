bf95d320ae7da135d3e844b87b89bad70226be21
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-03-30 11:58:27 -0300

    Test how the computer plays a turn

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -5,6 +5,14 @@ test("Real player plays one turn", () => {
   let real = new RealPlayer("Leon S Kennedy");
   let computer = new Computer();
   computer.gameboard.placeShip([0, 0], computer.gameboard.ships.carrier, "x");
-  real.playTurn([0, 0], computer);
+  real.playTurn(computer, [0, 0]);
   expect(computer.gameboard.board[0][0].ship.hitCount).toBe(1);
+});
+
+test("Computer plays one turn", () => {
+  let real = new RealPlayer("Leon S Kennedy");
+  let computer = new Computer();
+  real.gameboard.placeShip([0, 0], computer.gameboard.ships.carrier, "x");
+  computer.playTurn(real, [0, 0]);
+  expect(real.gameboard.board[0][0].ship.hitCount).toBe(1);
 });

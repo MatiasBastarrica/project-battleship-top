4f1248fe7f8658ea03dff789c9bbd9203e407a66
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-03-31 18:57:55 -0300

    If the user has submitted a name, start the game

--- src/DOM/dom.js
+++ src/DOM/dom.js
@@ -1,7 +1,16 @@
-import { populateHome } from "./pages/home.js";
+import { populateHome, getElements } from "./pages/home.js";
+import { Game } from "../game.js";
 
 export function populate(page) {
   if (page === "home") {
     populateHome();
+    const elements = getElements();
+    elements.startBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (elements.input.checkValidity) {
+        const game = new Game(elements.input.value);
+        console.log("game started");
+      }
+    });
   }
 }

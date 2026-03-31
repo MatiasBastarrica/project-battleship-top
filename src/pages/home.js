5893c4699d4fc11e7d61fd94fd36be10b74f8577
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-03-31 10:10:43 -0300

    Write the "home" page

---
+++ src/pages/home.js
@@ -0,0 +1,29 @@
+const page = document.createElement("div");
+page.classList.add("page");
+
+const h1 = document.createElement("h1");
+h1.textContent = "Battleship";
+h1.classList.add("title");
+page.appendChild(h1);
+
+const form = document.createElement("form");
+page.appendChild(form);
+
+const label = document.createElement("label");
+label.setAttribute("for", "player-name");
+form.appendChild(label);
+
+const input = document.createElement("input");
+input.setAttribute("type", "text");
+input.id = "player-name";
+input.required = true;
+form.appendChild(input);
+
+const startBtn = document.createElement("button");
+startBtn.textContent = "Start game";
+form.appendChild(startBtn);
+
+export function populate() {
+  const body = document.querySelector("body");
+  body.appendChild(page);
+}

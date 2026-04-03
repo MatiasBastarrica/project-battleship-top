0cb3c2f0a008b7414f01aa26c66d6f3f1f60ef33
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-04-03 15:53:26 -0300

    Load the "ship placement" page after the player submits a name

--- src/DOM/dom.js
+++ src/DOM/dom.js
@@ -1,16 +1,28 @@
 import { populateHome, getElements } from "./pages/home.js";
+import { populateShipPlacement } from "./pages/shipPlacement.js";
 import { Game } from "../game.js";
 
 export function populate(page) {
   if (page === "home") {
+    clearPage();
     populateHome();
     const elements = getElements();
     elements.startBtn.addEventListener("click", (e) => {
       e.preventDefault();
       if (elements.input.checkValidity) {
         const game = new Game(elements.input.value);
-        console.log("game started");
+        // console.log(`Playing as ${game.player1.name}`);
+        clearPage();
+        populateShipPlacement(game.player1.name);
       }
     });
+  }
+}
+
+function clearPage() {
+  const body = document.querySelector("body");
+  const firstChild = body.firstElementChild;
+  if (firstChild) {
+    body.removeChild(firstChild);
   }
 }

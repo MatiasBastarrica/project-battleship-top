f7853e3fadf9f800eebb797c2b29351cec7dd677
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-04-03 21:45:48 -0300

    Add the event listener for the button on this module

--- src/DOM/pages/home.js
+++ src/DOM/pages/home.js
@@ -1,3 +1,7 @@
+import { Game } from "../../game.js";
+import { clearPage } from "../dom.js";
+import { populateShipPlacement } from "./shipPlacement.js";
+
 const page = document.createElement("div");
 page.classList.add("page");
 
@@ -23,18 +27,27 @@ const startBtn = document.createElement("button");
 startBtn.textContent = "Start game";
 form.appendChild(startBtn);
 
+startBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (input.checkValidity) {
+    const game = new Game(input.value);
+    clearPage();
+    populateShipPlacement(game.player1.name, game);
+  }
+});
+
 export function populateHome() {
   const body = document.querySelector("body");
   body.appendChild(page);
 }
 
-export function getElements() {
-  return {
-    page,
-    h1,
-    form,
-    label,
-    input,
-    startBtn,
-  };
-}
+// export function getElements() {
+//   return {
+//     page,
+//     h1,
+//     form,
+//     label,
+//     input,
+//     startBtn,
+//   };
+// }

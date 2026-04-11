5ff15662d5fdced4023b4ca06c215cd93901ea15
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-04-11 11:21:46 -0300

    Play the game when the user clicks on the computer's board

--- src/DOM/pages/game-page.js
+++ src/DOM/pages/game-page.js
@@ -26,20 +26,24 @@ const player1Board = document.createElement("div");
 player1Board.classList.add("player1-board", "board");
 player1Container.appendChild(player1Board);
 
+const playerCells = [];
+
 for (let i = 0; i < 10; i++) {
+  let rowCells = [];
   const row = document.createElement("div");
   row.classList.add("row");
   player1Board.appendChild(row);
   for (let j = 0; j < 10; j++) {
     const col = document.createElement("div");
-    col.classList.add("cell");
+    col.classList.add("cell", "player-cell");
     col.dataset.cell = `${i},${j}`;
-    // addCellListeners(col);
+    rowCells.push(col);
     row.appendChild(col);
     const indicator = document.createElement("div");
     indicator.classList.add("indicator");
     col.appendChild(indicator);
   }
+  playerCells.push(rowCells);
 }
 
 const computerContainer = document.createElement("div");
@@ -54,29 +58,39 @@ const computerBoard = document.createElement("div");
 computerBoard.classList.add("computer-board", "board");
 computerContainer.appendChild(computerBoard);
 
+const computerCells = [];
+
 for (let i = 0; i < 10; i++) {
+  let rowCells = [];
   const row = document.createElement("div");
   row.classList.add("row");
   computerBoard.appendChild(row);
   for (let j = 0; j < 10; j++) {
     const col = document.createElement("div");
-    col.classList.add("cell");
+    col.classList.add("cell", "computer-cell");
     col.dataset.cell = `${i},${j}`;
-    // addCellListeners(col);
+    rowCells.push(col);
+    addCellListener(col);
     row.appendChild(col);
     const indicator = document.createElement("div");
     indicator.classList.add("indicator");
     col.appendChild(indicator);
   }
+  computerCells.push(rowCells);
 }
 
 let currentGame;
+let waitingStage = false;
 
 export function populateGamePage(game) {
   const body = document.querySelector("body");
   body.appendChild(page);
   currentGame = game;
+  //place the computer's ships at random coords
+  currentGame.computer.gameboard.placeFixed();
   updatePlayerBoard();
+  // console.log(currentGame.computer.gameboard.board);
+  // console.log(currentGame.player1.gameboard.board);
 }
 
 function updatePlayerBoard() {
@@ -89,7 +103,8 @@ function updatePlayerBoard() {
         currentGame.player1.gameboard.board[i][j].ship &&
         currentGame.player1.gameboard.board[i][j].alreadyAttacked
       ) {
-        const indicator = document.querySelector(".indicator");
+        const cell = playerCells[i][j];
+        const indicator = cell.querySelector(".indicator");
         indicator.classList.add("hit-indicator");
         indicator.style.display = "block";
       }
@@ -97,7 +112,8 @@ function updatePlayerBoard() {
         !currentGame.player1.gameboard.board[i][j].ship &&
         currentGame.player1.gameboard.board[i][j].alreadyAttacked
       ) {
-        const indicator = document.querySelector(".indicator");
+        const cell = playerCells[i][j];
+        const indicator = cell.querySelector(".indicator");
         indicator.classList.add("water-indicator");
         indicator.style.display = "block";
       }
@@ -105,16 +121,9 @@ function updatePlayerBoard() {
         currentGame.player1.gameboard.board[i][j].ship &&
         !currentGame.player1.gameboard.board[i][j].alreadyAttacked
       ) {
-        const cell = document.querySelector(`[data-cell = "${i},${j}"]`);
+        const cell = playerCells[i][j];
         cell.classList.add("ship-cell");
       }
-      // if (
-      //   currentGame.player1.gameboard.board[i][j].ship &&
-      //   !currentGame.player1.gameboard.board[i][j].alreadyAttacked
-      // ) {
-      //   const cell = document.querySelector(`[data-cell = "${i},${j}"]`);
-      //   cell.classList.add("ship-cell");
-      // }
     }
   }
 }
@@ -129,7 +138,8 @@ function updateComputerBoard() {
         currentGame.computer.gameboard.board[i][j].ship &&
         currentGame.computer.gameboard.board[i][j].alreadyAttacked
       ) {
-        const indicator = document.querySelector(".indicator");
+        const cell = computerCells[i][j];
+        const indicator = cell.querySelector(".indicator");
         indicator.classList.add("hit-indicator");
         indicator.style.display = "block";
       }
@@ -137,10 +147,32 @@ function updateComputerBoard() {
         !currentGame.computer.gameboard.board[i][j].ship &&
         currentGame.computer.gameboard.board[i][j].alreadyAttacked
       ) {
-        const indicator = document.querySelector(".indicator");
+        const cell = computerCells[i][j];
+        const indicator = cell.querySelector(".indicator");
         indicator.classList.add("water-indicator");
         indicator.style.display = "block";
       }
     }
   }
+}
+
+function addCellListener(cell) {
+  cell.addEventListener("click", (e) => {
+    if (!currentGame.over && !waitingStage) {
+      const row = e.currentTarget.dataset.cell[0];
+      const col = e.currentTarget.dataset.cell[2];
+      if (!currentGame.computer.gameboard.board[row][col].alreadyAttacked) {
+        currentGame.play([row, col]);
+        updateComputerBoard();
+        waitingStage = true;
+        if (!currentGame.over) {
+          setTimeout(() => {
+            currentGame.play();
+            updatePlayerBoard();
+            waitingStage = false;
+          }, 2000);
+        }
+      }
+    }
+  });
 }
